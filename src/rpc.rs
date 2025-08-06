b1bcbeaf18ce682d3c81429f20e0da83d81109f8
Lenard0 <lachie.hislop@gmail.com>
2025-08-06 11:46:16 +0930

    added rpc function calls

--- src/rpc.rs
+++ src/rpc.rs
@@ -1,34 +1,24 @@
-use serde::{de::DeserializeOwned, Serialize};
 use crate::{universals::HttpMethod, SupabaseClient};
 
 
 impl SupabaseClient {
-    pub async fn rpc_call<Input, Output>(
+    /// Call a Supabase RPC with JSON input and get JSON array output.
+    pub async fn rpc_call(
         &self,
         rpc_name: &str,
-        args: &Input,
-    ) -> Result<Vec<Output>, String>
-    where
-        Input: Serialize + ?Sized,
-        Output: DeserializeOwned,
-    {
+        args: serde_json::Value,  // JSON object with RPC args
+    ) -> Result<Vec<serde_json::Value>, String> {
         let path = format!("/rest/v1/rpc/{}", rpc_name);
 
-        // Serialize args to JSON inside request method
         let response = self
-            .request(&path, HttpMethod::Post, Some(serde_json::to_value(args).map_err(|e| e.to_string())?), false)
+            .request(&path, HttpMethod::Post, Some(args), false)
             .await?;
 
+        // Expect response to be a JSON array
         let arr = response
             .as_array()
             .ok_or_else(|| "Expected JSON array from RPC".to_string())?;
 
-        let result = arr
-            .iter()
-            .map(|item| serde_json::from_value(item.clone()))
-            .collect::<Result<Vec<Output>, _>>()
-            .map_err(|e| format!("Deserialization error: {:?}", e))?;
-
-        Ok(result)
+        Ok(arr.clone())
     }
 }
